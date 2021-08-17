4dceec66b434931e2f82224d35047fb6a8082eca
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2021-08-17 23:23:53 +0900

    update_210817
    PUG -> IF, Else Use (fakeUser)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,12 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+const fakeUser = {
+    username: "ShinBi",
+    loggedIn: false
+}
+
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch"});
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit"  });
 export const search = (req, res) => res.send("Search Video");
 export const deleteVideo = (req, res) => {
    
