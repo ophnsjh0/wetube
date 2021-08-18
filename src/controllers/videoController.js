525b5a141b6211f9379b388ca502f40557775075
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2021-08-18 23:26:49 +0900

    update_210818

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,27 @@
 export const trending = (req, res) => {
-    const videos = [];
+    const videos = [
+        {
+            title: "First Video",
+            rating : 5,
+            comment : "good Video",
+            createdAt : 15,
+            views : 392
+        },
+        {
+            title: "Second Video",
+            rating : 1,
+            comment : "sad Video",
+            createdAt : 2,
+            views : 200003
+        },
+        {
+            title: "Third Video",
+            rating : 5,
+            comment : "great Video",
+            createdAt : 300,
+            views : 10002342
+        },
+    ];
     return res.render("home", {pageTitle: "Home", videos });
 };
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch"});
