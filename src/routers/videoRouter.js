50ba84cbcbccdcc8bb5eae301cde345a9b8d7fcd
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2021-08-24 00:16:14 +0900

    update_210824

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
-import {watch, edit, deleteVideo, upload} from "../controllers/videoController"
+import {watch, getEdit, postEdit} from "../controllers/videoController"
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+// videoRouter.get("/:id(\\d+)/edit", getEdit);
+// videoRouter.post("/:id(\\d+)/edit", postEdit);
+
 
 export default videoRouter;
 
--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + '/src/views')
 app.use(logger);
+app.use(express.urlencoded({ extended: true })); //URL 인코더 미들웨어 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
