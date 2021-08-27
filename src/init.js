1d1e3ab43b710d295dd781787ee8351dd45b9a99
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2021-08-28 08:42:21 +0900

    update_20210828

---
+++ src/init.js
@@ -0,0 +1,9 @@
+import "./db.js";
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`🆗 Server listenting on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
