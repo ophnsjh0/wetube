57f3ff044e30fd02fdbf1637a3c985256ed3cec2
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2021-11-08 23:41:14 +0900

    comiit_20211108

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle : "Join" });
 export const postJoin = async(req, res) => {
@@ -13,6 +14,7 @@ export const postJoin = async(req, res) => {
         return res.status(400).render("join", {pageTitle,
         errorMessage : " This username/email is already taken.",});
     };
+
     // const usernameExists = await User.exists({ username });
     // if (usernameExists){
     //     return res.render("join", { pageTitle, 
@@ -41,14 +43,21 @@ export const postJoin = async(req, res) => {
 export const getLogin = (req, res) => res.render("login", {pageTitle : "Login"});
 export const postLogin = async (req, res) => {
     const { username, password } = req.body
-    const exists = await User.exists({username});
-    if(!exists){
-        return res.status(400).render("login", {pageTitle: "Login",
+    const user = await User.findOne({ username });
+    const pageTitle = "Login";
+    // const exists = await User.exists({username});
+    // check if account exists
+    if(!user){
+        return res.status(400).render("login", {pageTitle,
         errorMessage : " An account with this username does not exists.",});
     };
-    // check if account exists
     // check if password correct
-    res.end();
+    const ok = await bcrypt.compare(password, user.password);
+    if(!ok){
+        return res.status(400).render("login", {pageTitle,
+        errorMessage : " Wrong password.",});
+    };
+    return res.redirect(`/`);
 };
 
 
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import morgan from "morgan"
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import session from "express-session";
 
 
 
@@ -16,6 +17,28 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + '/src/views')
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); //URL 인코더 미들웨어 
+app.use(session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+    })
+);
+// cookie 정보
+// app.use((req, res, next) => {
+//     console.log(req.headers);
+//     next();
+// });
+// session 정보 
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        req.session.potato += 1;
+        console.log(sessions);
+        next();
+    })
+});
+app.get("/add-one", (req, res, next) => {
+    return res.send(`${req.session.id}`);
+});
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
